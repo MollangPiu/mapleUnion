7a8111c3c7821f2d4be905439e7497feb14cbb65
MHS <hayaruby7909@gmail.com>
2023-12-31 21:51:52 +0900

    Feat: Member 테이블 정의

--- src/main/java/soft/kr/maple/controller/MemberController.java
+++ src/main/java/soft/kr/maple/controller/MemberController.java
@@ -1,10 +1,7 @@
 package soft.kr.maple.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import soft.kr.maple.service.MemberService;
 
 @RestController
@@ -19,8 +16,9 @@ public class MemberController {
      * 회원가입, 아이디 중복 체크
      */
     @PostMapping("/join/findId")
-    public void MemberJoinFindId() {
+    public void MemberJoinFindId(@RequestBody @RequestParam(value="id", required = false) String id) {
         System.out.println("확인");
+        System.out.println(id);
     }
     
     @GetMapping("/find")
