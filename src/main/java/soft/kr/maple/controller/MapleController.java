f14fd9307accdd4d0a2bcc7316b955692b907ce2
MHS <hayaruby7909@gmail.com>
2024-01-06 02:21:48 +0900

    Update: JPA - MARIADB 설정

--- src/main/java/soft/kr/maple/controller/MapleController.java
+++ src/main/java/soft/kr/maple/controller/MapleController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MapleController {
 
 
-    /*@PersistenceContext
-    EntityManager entityManager;
-*/
+
+
     @GetMapping("/test")
     public void test() {
 
